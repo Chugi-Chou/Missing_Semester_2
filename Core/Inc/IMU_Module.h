daf5c256cf34acabc132fe9973abf52087a11bfc
zhou_zhi <zhou_zhi@sjtu.edu.cn>
2025-10-17 18:14:05 +0800

    Add: Creating the IMU class files without implement

---
+++ Core/Inc/IMU_Module.h
@@ -0,0 +1,8 @@
+//
+// Created by zhouzhi on 2025/10/17.
+//
+
+#ifndef IMU_MODULE_H
+#define IMU_MODULE_H
+
+#endif //IMU_MODULE_H
---
+++ Core/Src/IMU_Module.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zhouzhi on 2025/10/17.
+//
+
+#include "IMU_Module.h"
